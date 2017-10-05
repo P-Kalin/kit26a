27f39f34dc63e49d8664bc2c1713a92f3cf31d86
malokhvii-eduard <malokhvii.ee@gmail.com>
2017-10-06 00:50:15 +0300

    #2 malokhvii05: move bubble sort implementation from ArraySortUtils to simple BubbleSort object

---
+++ malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/algorithms/sort/BubbleSort.java
@@ -0,0 +1,35 @@
+package ua.khpi.oop.malokhvii05.util.algorithms.sort;
+
+import java.util.Comparator;
+
+import ua.khpi.oop.malokhvii05.util.Array;
+
+public class BubbleSort<T extends Comparable<T>> extends AbstractSortAlgorithm<T> {
+
+    public BubbleSort(Comparator<T> comparator) {
+        super(comparator);
+    }
+
+    @Override
+    public void sort(Array<T> array) {
+        int externalLoopBarrier = array.size();
+        boolean isSwapped;
+
+        int i;
+        int j;
+        for (i = 0; i < externalLoopBarrier; i++) {
+            isSwapped = false;
+            for (j = 0; j < externalLoopBarrier - i - 1; j++) {
+                if ((comparator.compare(array.get(j),
+                        array.get(j + 1)) == sortOrderKey)) {
+                    swap(array, j, j + 1);
+                    isSwapped = true;
+                }
+            }
+
+            if (isSwapped == false) {
+                break;
+            }
+        }
+    }
+}
