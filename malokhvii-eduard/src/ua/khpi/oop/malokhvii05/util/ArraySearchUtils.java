c6168dfa4e121b50ef499716c0af0e2da5dbaa17
malokhvii-eduard <malokhvii.ee@gmail.com>
2017-10-05 04:12:26 +0300

    #1 malokhvii05: implement gallop search

--- malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/ArraySearchUtils.java
+++ malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/ArraySearchUtils.java
@@ -6,6 +6,86 @@ public final class ArraySearchUtils {
 
     public static final int INDEX_NOT_FOUND = -1;
 
+    public static <T extends Comparable<T>> int gallopSearch(Array<T> array,
+            T value) {
+        return gallopSearch(array, value, 0, array.size());
+    }
+
+    public static <T> int gallopSearch(Array<T> array, T value,
+            Comparator<T> comparator) {
+        return gallopSearch(array, value, 0, array.size(), comparator);
+    }
+
+    public static <T extends Comparable<T>> int gallopSearch(Array<T> array,
+            T value, int left, int right) {
+        if (array.isEmpty()) {
+            return INDEX_NOT_FOUND;
+        }
+
+        int offset = 1;
+        T currentValue;
+        int nextLeftValue;
+        jump: while (left <= right) {
+            currentValue = array.get(left);
+            if (currentValue.compareTo(value) == 0) {
+                return left;
+            }
+
+            nextLeftValue = left + offset;
+            if ((currentValue.compareTo(value) == -1)
+                    || nextLeftValue > right) {
+                if (currentValue.compareTo(value) == 1) {
+                    right = left;
+                }
+
+                left -= (offset >> 1);
+                ++left;
+                offset = 1;
+                continue jump;
+            }
+
+            left = nextLeftValue;
+            offset <<= 1;
+        }
+
+        return INDEX_NOT_FOUND;
+    }
+
+    public static <T> int gallopSearch(Array<T> array, T value, int left,
+            int right, Comparator<T> comparator) {
+        if (array.isEmpty()) {
+            return INDEX_NOT_FOUND;
+        }
+
+        int offset = 1;
+        T currentValue;
+        int nextLeftValue;
+        jump: while (left <= right) {
+            currentValue = array.get(left);
+            if (comparator.compare(currentValue, value) == 0) {
+                return left;
+            }
+
+            nextLeftValue = left + offset;
+            if ((comparator.compare(currentValue, value) == -1)
+                    || nextLeftValue > right) {
+                if (comparator.compare(currentValue, value) == 1) {
+                    right = left;
+                }
+
+                left -= (offset >> 1);
+                ++left;
+                offset = 1;
+                continue jump;
+            }
+
+            left = nextLeftValue;
+            offset <<= 1;
+        }
+
+        return INDEX_NOT_FOUND;
+    }
+
     public static <T extends Number> int interpolationSearch(Array<T> array,
             T value) {
         return interpolationSearch(array, value, 0, array.size());
