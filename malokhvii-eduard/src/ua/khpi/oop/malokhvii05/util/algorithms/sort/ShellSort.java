a1c8af5056ef9a70098cca7d72ec8724a76031b2
malokhvii-eduard <malokhvii.ee@gmail.com>
2017-10-06 00:54:22 +0300

    #2 malokhvii05: move shell sort implementation from ArraySortUtils to simple ShellSort object

---
+++ malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/algorithms/sort/ShellSort.java
@@ -0,0 +1,32 @@
+package ua.khpi.oop.malokhvii05.util.algorithms.sort;
+
+import java.util.Comparator;
+
+import ua.khpi.oop.malokhvii05.util.Array;
+
+public class ShellSort<T> extends AbstractSortAlgorithm<T> {
+
+    public ShellSort(Comparator<T> comparator) {
+        super(comparator);
+    }
+
+    @Override
+    public void sort(Array<T> array) {
+        int arraySize = array.size();
+        T comparableValue;
+        int gap;
+
+        int i;
+        int j;
+        for (gap = arraySize >> 1; gap > 0; gap >>= 1) {
+            for (i = gap; i < arraySize; i++) {
+                comparableValue = array.get(i);
+                for (j = i; j >= gap && comparator.compare(array.get(j - gap),
+                        comparableValue) == sortOrderKey; j -= gap) {
+                    array.set(j, array.get(j - gap));
+                }
+                array.set(j, comparableValue);
+            }
+        }
+    }
+}
