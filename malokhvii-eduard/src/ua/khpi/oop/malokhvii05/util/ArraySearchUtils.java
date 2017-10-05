77054376a53eea4aaaa82b50c4bef86b82707ced
malokhvii-eduard <malokhvii.ee@gmail.com>
2017-10-05 04:11:31 +0300

    #1 malokhvii05: implement interpolation search

--- malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/ArraySearchUtils.java
+++ malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/ArraySearchUtils.java
@@ -6,6 +6,57 @@ public final class ArraySearchUtils {
 
     public static final int INDEX_NOT_FOUND = -1;
 
+    public static <T extends Number> int interpolationSearch(Array<T> array,
+            T value) {
+        return interpolationSearch(array, value, 0, array.size());
+    }
+
+    public static <T extends Number> int interpolationSearch(Array<T> array,
+            T value, int left, int right) {
+        T leftValue;
+        T rightValue;
+        T middleValue;
+
+        int arraySize = array.size();
+
+        float average;
+        int difference;
+        int steps;
+        int middle;
+        while (left < right && left >= 0 && right <= arraySize) {
+            leftValue = array.get(left);
+            rightValue = array.get(right - 1);
+
+            average = ((float) rightValue.intValue() - leftValue.intValue())
+                    / (right - left);
+            middle = left;
+            if (average != 0) {
+                difference = value.intValue() - leftValue.intValue();
+                steps = (int) (difference / average);
+                middle = left + steps;
+
+                if (middle >= right) {
+                    middle = (left + right) / 2;
+                }
+            }
+
+            middleValue = array.get(middle);
+            if (middleValue.equals(value)) {
+                return middle;
+            } else if (value.intValue() < middleValue.intValue()) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        if (array.get(left).equals(value)) {
+            return left;
+        }
+
+        return INDEX_NOT_FOUND;
+    }
+
     public static <T extends Comparable<T>> int linearSearch(Array<T> array,
             T value) {
         int index;
