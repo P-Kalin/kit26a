3c604609e1e1b317b8009fb35f1967785fd0aaf6
malokhvii-eduard <malokhvii.ee@gmail.com>
2017-10-06 00:53:19 +0300

    #2 malokhvii05: move selection sort implementation from ArraySortUtils to simple SelectionSort object

---
+++ malokhvii-eduard/src/ua/khpi/oop/malokhvii05/util/algorithms/sort/SelectionSort.java
@@ -0,0 +1,40 @@
+package ua.khpi.oop.malokhvii05.util.algorithms.sort;
+
+import java.util.Comparator;
+
+import ua.khpi.oop.malokhvii05.util.Array;
+
+public final class SelectionSort<T> extends AbstractSortAlgorithm<T> {
+
+    private boolean isReversed;
+
+    public SelectionSort(Comparator<T> comparator) {
+        super(comparator);
+    }
+
+    @Override
+    public void setSortOrder(SortOrder sortOrder) {
+        isReversed = isReversed(sortOrderToKey(sortOrder),
+                INTERNAL_ASCENDING_KEY);
+        super.setSortOrder(sortOrder);
+    }
+
+    @Override
+    public void sort(Array<T> array) {
+        int arraySize = array.size();
+
+        int minimumIndex;
+        int i;
+        int j;
+        for (i = 0; i < arraySize - 1; i++) {
+            minimumIndex = i;
+            for (j = i + 1; j < arraySize; j++) {
+                if (isReversed == (comparator.compare(array.get(j),
+                        array.get(minimumIndex)) < sortOrderKey)) {
+                    minimumIndex = j;
+                }
+            }
+            swap(array, minimumIndex, i);
+        }
+    }
+}
