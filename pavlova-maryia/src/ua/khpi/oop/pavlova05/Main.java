5a1235688e15ff7c2b2ea4ef92d9edff9bb3f7a6
Maryia Pavlova <noLegacyIsSoRichAsHonesty@ukr.net>
2017-10-21 19:03:41 +0300

    #2 pavlova05: rewrite solution of the main task

--- pavlova-maryia/src/ua/khpi/oop/pavlova05/Main.java
+++ pavlova-maryia/src/ua/khpi/oop/pavlova05/Main.java
@@ -15,7 +15,6 @@ public class Main {
 		int number = (12 % 10) + 1;
 		System.out.println(number);
 
-		ContainerOfStrings container = new ContainerOfStrings();
 		String text;
 		StringBuilder stringBuilder = new StringBuilder();
 
@@ -27,23 +26,23 @@ public class Main {
 			stringBuilder.append(text);
 		}
 		text = stringBuilder.toString();
-
+		NewContainerOfStrings containerOfStrings = new NewContainerOfStrings();
 		Iterable<String> sentences = TextUtil.extractAllSentences(text);
 		for (String sentence : sentences)
-			container.add(sentence);
-
-		ContainerOfStrings longest = (ContainerOfStrings) TextUtil.findLongestWordsInLines(sentences);
-		ContainerOfStrings shortest = (ContainerOfStrings) TextUtil.findShortestWordsInLines(sentences);
-
-		System.out.println("Список найдовших слів");
-		ContainerOfStrings.IteratorForContainer<String> iteratorForContainerLongest = longest.iterator();
+			containerOfStrings.add(sentence);
+		NewContainerOfStrings longest = new NewContainerOfStrings(containerOfStrings.size());
+		NewContainerOfStrings shortest = new NewContainerOfStrings(containerOfStrings.size());
+		for (String string : containerOfStrings.elementData) {
+			longest.add(TextUtil.findLongestWordtInText(string));
+			shortest.add(TextUtil.findShortestWordInText(string));
+		}
+		NewContainerOfStrings.newIterator<String> iteratorForContainerLongest = longest.iterator();
 		while (iteratorForContainerLongest.hasNext()) {
 			System.out.println(iteratorForContainerLongest.next());
 		}
-		System.out.println("Список найкоротших слів");
-		for (String shortWord : shortest) {
-			System.out.println(shortWord);
+		NewContainerOfStrings.newIterator<String> iteratorForContainerShortest = shortest.iterator();
+		while (iteratorForContainerShortest.hasNext()) {
+			System.out.println(iteratorForContainerShortest.next());
 		}
-
 	}
 }
