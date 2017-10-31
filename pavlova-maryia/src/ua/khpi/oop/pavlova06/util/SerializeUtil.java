4d84b085e759614c2a9ed7fc30ca0d921e514f11
Maryia Pavlova <noLegacyIsSoRichAsHonesty@ukr.net>
2017-11-01 02:31:53 +0300

    #2 pavlova06: add comments

--- pavlova-maryia/src/ua/khpi/oop/pavlova06/util/SerializeUtil.java
+++ pavlova-maryia/src/ua/khpi/oop/pavlova06/util/SerializeUtil.java
@@ -11,12 +11,27 @@ import java.io.ObjectOutputStream;
 import ua.khpi.oop.malokhvii05.util.Array;
 import ua.khpi.oop.pavlova06.NewContainerOfStrings;
 
+/**
+ * Class <b>SerializeUtil</b> is an util class for serialization and
+ * deserialization.
+ * 
+ * @author pavlova-mv
+ *
+ */
 public class SerializeUtil {
+	/**
+	 * Method <b>serialize is for serialization container with NewContainerOfStrings
+	 * type.</br>
+	 * It writes the information about an object in the special file.
+	 * 
+	 * @param sentences
+	 *            container of sentences.
+	 */
 	public static void serialize(NewContainerOfStrings sentences) {
 		ObjectOutputStream out = null;
 		try {
 
-			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Serial")));
+			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("SerialCont")));
 
 			out.writeObject(sentences);
 			System.out.println("	Записано: " + sentences);
@@ -33,11 +48,19 @@ public class SerializeUtil {
 		}
 	}
 
+	/**
+	 * Method <b>serialize is for serialization of container with Array<String>
+	 * type.</br>
+	 * It writes the information about an object in the special file.
+	 * 
+	 * @param sentences
+	 *            container of sentences.
+	 */
 	public static void serialize(Array<String> sentences) {
 		ObjectOutputStream out = null;
 		try {
 
-			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("Data.ser")));
+			out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("SerialArr")));
 
 			out.writeObject(sentences);
 			System.out.println("	Записано: " + sentences);
@@ -54,20 +77,26 @@ public class SerializeUtil {
 		}
 	}
 
+	/**
+	 * Method <b>deserialize</b> is for deserialization of an object with a type
+	 * NewContainerOfStrings from the special file.
+	 * 
+	 * @return object
+	 */
 	public static NewContainerOfStrings deserializeCont() {
 		NewContainerOfStrings sentences = null;
 		ObjectInputStream in = null;
 		try {
 
-			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("Serial")));
+			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("SerialCont")));
 
 			sentences = (NewContainerOfStrings) in.readObject();
-			System.out.println("	Зчитано: " + sentences);
+			System.out.println("	Cчитано: " + sentences);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			/* Обов'язково зачиняємо потік */
+
 		} finally {
 			if (in != null)
 				try {
@@ -79,16 +108,22 @@ public class SerializeUtil {
 		return sentences;
 	}
 
+	/**
+	 * Method <b>deserialize</b> is for deserialization of an object with a type
+	 * Array<String> from the special file.
+	 * 
+	 * @return object
+	 */
 	@SuppressWarnings("unchecked")
 	public static Array<String> deserialize() {
 		Array<String> sentences = null;
 		ObjectInputStream in = null;
 		try {
 
-			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("Serial")));
+			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("SerialArr")));
 
 			sentences = (Array<String>) in.readObject();
-			System.out.println("	Зчитано: " + sentences);
+			System.out.println("	Cчитано: " + sentences);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} catch (Exception ex) {
