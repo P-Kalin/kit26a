914186652fb771f978914de3db8aa9f96fff2ed2
Maryia Pavlova <noLegacyIsSoRichAsHonesty@ukr.net>
2017-12-09 16:11:02 +0200

    #1 pavlova14: add draft

---
+++ pavlova-maryia/src/ua/khpi/oop/pavlova14/ExtraFunctions.java
@@ -0,0 +1,21 @@
+package ua.khpi.oop.pavlova14;
+
+import ua.khpi.oop.pavlova10.HotelGuest;
+import ua.khpi.oop.pavlova10.LinkedList;
+
+public class ExtraFunctions {
+	public static LinkedList<HotelGuest> createDefaultList() {
+		LinkedList<HotelGuest> list = new LinkedList<HotelGuest>();
+		HotelGuest one = new HotelGuest("Иванов А.Р.", "23-Октябрь-1980", "Львов, Украина", "МТ-452178", "12-Июнь-2016",
+				"23-Июнь-2016", "123", "Бизнес", "2", "Командировка");
+		HotelGuest two = new HotelGuest("Сидоров Л.Ф.", "11-Май-1991", "Ивано-Франковск, Украина", "МТ-123654",
+				"15-Сентябрь-2017", "22-Сентябрь-2017", "220", "Эконом", "1", "Туризм");
+		HotelGuest three = new HotelGuest("Петров В.В.", "14-Декабрь-1989", "Полтава, Украина", "МТ-546985",
+				"28-Апрель-2017", "5-Май-2017", "51", "Бизнес", "3", "Командировка");
+		list.add(one);
+		list.add(two);
+		list.add(three);
+
+		return list;
+	}
+}
---
+++ pavlova-maryia/src/ua/khpi/oop/pavlova14/Main.java
@@ -0,0 +1,25 @@
+package ua.khpi.oop.pavlova14;
+
+public class Main {
+	/** Кількість мілісекунд в одній наносекунді. */
+	private static final double DIVIDER = 1_000_000;
+	private static long start;
+
+	public static void main(final String[] args) {
+
+		System.out.println("Parallel processing");
+		start = System.nanoTime();
+		Demonstration.demonstrateSequential();
+		;
+		countTime(start);
+
+		System.out.println("Sequential processing");
+		start = System.nanoTime();
+		Demonstration.demonstrateParallel();
+		countTime(start);
+	}
+
+	private static void countTime(long start) {
+		System.out.println("Время обработки: " + (System.nanoTime() - start) / DIVIDER);
+	}
+}
