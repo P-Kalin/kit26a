795a94753afc005bbcc8e4f7d20a24519f1380f4
Maryia Pavlova <noLegacyIsSoRichAsHonesty@ukr.net>
2017-12-10 18:46:12 +0200

    #2 pavlova15: remove wrong throw exceptions

--- pavlova-maryia/src/ua/khpi/oop/pavlova15/Main.java
+++ pavlova-maryia/src/ua/khpi/oop/pavlova15/Main.java
@@ -1,13 +1,12 @@
 package ua.khpi.oop.pavlova15;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import ua.khpi.oop.pavlova10.HotelGuest;
 
 public class Main {
-	public static void main(final String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
+	public static void main(final String[] args) throws FileNotFoundException {
 		ArrayList<HotelGuest> arrayList = ExtraFunctions.createDefaultArrayList();
 		CommandParser.doCommand(arrayList);
 
